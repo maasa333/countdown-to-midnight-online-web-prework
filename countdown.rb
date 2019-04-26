005229ae03605c7153be18db17491cadfea7a29e
Maasa Kono <konomaasa@gmail.com>
2019-04-26 20:01:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,15 @@
 
 def countdown
   x = 10
+  while x > 1
     puts "#{x} SECONDS!"
-    while x > 1
     x -= 1
+  end
   while x == 1
       puts "#{x} SECOND!"
       x -= 1
+  end
   while x == 0
       puts "HAPPY NEW YEAR!"
-    end
-  end
   end
 end
