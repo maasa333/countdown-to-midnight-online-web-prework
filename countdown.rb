1daa1d80b9b35f80f0e2b03277ec2dd08eacaaab
Maasa Kono <konomaasa@gmail.com>
2019-04-26 19:33:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,12 @@
 
 def countdown
   x = 10
-  while x > -1
-    puts "#{x} NUMBER OF SECONDS!"
+  while x > 1
+    puts "#{x} SECONDS!"
     x -= 1
+    if x == 1
+      puts "#{x} SECOND!"
+    else
+      
 end
 end
